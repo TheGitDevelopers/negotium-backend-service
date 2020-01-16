099d23bd75e5c30871ec99d83881978ee8a03584
Mateusz <mateusz.chojnowski@asseco.pl>
2020-01-16 22:49:53 +0100

    GLobalne logi

--- src/main/java/com/negotium/negotiumapp/NegotiumappApplication.java
+++ src/main/java/com/negotium/negotiumapp/NegotiumappApplication.java
@@ -1,13 +1,26 @@
 package com.negotium.negotiumapp;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.SimpleFormatter;
 
 @SpringBootApplication
+@RestController
 public class NegotiumappApplication {
 
-    public static void main(String[] args) {
+    private static final Logger logger = LoggerFactory.getLogger(NegotiumappApplication.class);
+
+
+    public static void main(String[] args)
+    {
         SpringApplication.run(NegotiumappApplication.class, args);
+
     }
 
 }
