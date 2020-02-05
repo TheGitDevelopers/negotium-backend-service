93d7bcfbc5e2ca7c91eb424b515a470659b0d7ed
Robert Prus <robert.prus87@gmail.com>
2020-02-05 22:58:39 +0100

    Refactoring code EmployeeMapper

--- src/main/java/com/negotium/negotiumapp/model/employee/EmployeeMapper.java
+++ src/main/java/com/negotium/negotiumapp/model/employee/EmployeeMapper.java
@@ -1,24 +1,28 @@
 package com.negotium.negotiumapp.model.employee;
 
-public class EmployeeMapper {
+//@Mapper
+public interface EmployeeMapper {
 
-    public static EmployeeDto toDto(Employee employee){
+//    EmployeeMapper INSTANCE = Mappers.getMapper(EmployeeMapper.class);
+//
+//    EmployeeDto toDTO(Employee entity);
+//    Employee toEntity(EmployeeDto dto);
+
+    public static EmployeeDto toDto(Employee employee) {
         EmployeeDto dto = new EmployeeDto();
         dto.setId(employee.getId());
         dto.setName(employee.getName());
         dto.setEmployeeIndex(employee.getEmployeeIndex());
         dto.setEmployeeDetails(employee.getEmployeeDetails());
-        dto.setEmployeeDetails(employee.getEmployeeDetails());
         return dto;
     }
 
-    public static Employee toEntity(EmployeeDto employee){
+    public static Employee toEntity(EmployeeDto employee) {
         Employee entity = new Employee();
         entity.setId(employee.getId());
         entity.setName(employee.getName());
         entity.setEmployeeDetails(employee.getEmployeeDetails());
         employee.setEmployeeIndex(employee.getEmployeeIndex());
-        entity.setEmployeeDetails(employee.getEmployeeDetails());
         return entity;
     }
 }
