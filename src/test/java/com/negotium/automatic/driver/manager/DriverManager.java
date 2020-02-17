fe82d37a163310c4d7ee1da850bed14a1eb7236a
Averthel <thecojove@gmail.com>
2020-02-17 22:26:01 +0100

    Update

--- src/test/java/com/negotium/automatic/driver/manager/DriverManager.java
+++ src/test/java/com/negotium/automatic/driver/manager/DriverManager.java
@@ -1,8 +1,11 @@
 package com.negotium.automatic.driver.manager;
 
-import com.negotium.automatic.configuration.LocalWebDriverProperties;
 import com.negotium.automatic.driver.BrowserFactory;
 import org.openqa.selenium.WebDriver;
+
+import static com.negotium.automatic.configuration.TestRunProperties.getBrowserToRun;
+import static com.negotium.automatic.configuration.TestRunProperties.getIsRemoteRun;
+import static com.negotium.automatic.driver.BrowserType.FIREFOX;
 
 public class DriverManager {
 
@@ -13,14 +16,14 @@ public class DriverManager {
 
     public static WebDriver getWebDriver() {
         if (driver != null) {
-            driver = BrowserFactory.getBrowser(LocalWebDriverProperties.getLocalBrowser());
+            driver = new BrowserFactory(getBrowserToRun(), getIsRemoteRun()).getBrowser();
         }
         return driver;
     }
 
     public static void disposeDriver() {
         driver.close();
-        if (!LocalWebDriverProperties.getLocalBrowser().equals(LocalWebDriverProperties.getFirefoxWebDriverLocation())){
+        if (!getBrowserToRun().equals(FIREFOX)){
             driver.quit();
         }
         driver = null;
