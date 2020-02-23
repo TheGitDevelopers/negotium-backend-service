b1fca51017813f45b0f72d3e4a8880fcf9c56ae1
Robert Prus <robert.prus87@gmail.com>
2020-02-23 21:01:07 +0100

    Repairing test - changing returning object type of EmployeeRepository method findAllByName

--- src/main/java/com/negotium/negotiumapp/repository/EmployeeRepository.java
+++ src/main/java/com/negotium/negotiumapp/repository/EmployeeRepository.java
@@ -4,11 +4,14 @@ import com.negotium.negotiumapp.model.user.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Optional<Employee> findByEmployeeIndex(int employeeIndex);
-    Optional<Employee> findAllByName(String name);
+
+    List<Employee> findAllByName(String name);
+
     Employee findOneByName(String name);
 }
