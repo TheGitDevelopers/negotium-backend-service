25a9f01ad0e1607a926a795387dcc7bdcff9edd9
Robert Prus <robert.prus87@gmail.com>
2020-05-27 13:34:54 +0200

    Deleted unused builders customization lines.

--- src/main/java/com/negotium/negotiumapp/security/config/date/NegotiumDateTimeConfig.java
+++ src/main/java/com/negotium/negotiumapp/security/config/date/NegotiumDateTimeConfig.java
@@ -1,14 +1,11 @@
 package com.negotium.negotiumapp.security.config.date;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Configuration
@@ -20,14 +17,8 @@ public class NegotiumDateTimeConfig {
     public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
         return builder -> {
             builder.simpleDateFormat(dateTimeFormat);
-            builder.serializerByType(LocalDateTime.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(dateFormat)));
             builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(dateFormat)));
-//            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimeFormat)));
-
-            builder.deserializerByType(LocalDateTime.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(dateFormat)));
-            builder.serializationInclusion(JsonInclude.Include.NON_NULL);
-            builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-            
+            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimeFormat)));
         };
     }
 }
